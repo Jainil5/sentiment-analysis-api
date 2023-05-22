a9ec30f0a61596d7e230ec3eebfc24c898fdd5b0
Jainil5 <jainiljp72525@gmail.com>
2023-05-23 01:55:03 +0530

    initial commit

--- api.py
+++ api.py
@@ -1,6 +1,5 @@
 import requests
 from flask import Flask
-<<<<<<< HEAD
 from flask_restful import Api,Resource
 from transformers import AutoTokenizer
 from transformers import AutoModelForSequenceClassification
@@ -8,13 +7,11 @@ import torch
 import tensorflow as tf
 from tensorflow import keras
 from keras.preprocessing.text import Tokenizer
-=======
 from flask_restful import Api,Resource,reqparse
 from transformers import AutoTokenizer
 from transformers import AutoModelForSequenceClassification
 import torch
 
->>>>>>> cf5e1c883e2a08a149b2e3b2dcd36db8c7a7517f
 
 app = Flask(__name__)
 api = Api(app)
@@ -23,7 +20,6 @@ import_model = "cardiffnlp/twitter-xlm-roberta-base-sentiment-multilingual"
 tokenizer  = AutoTokenizer.from_pretrained(import_model)
 model =AutoModelForSequenceClassification.from_pretrained(import_model)
 
-<<<<<<< HEAD
 ###  Tokenization
 
 # sentences = ["I love my dog","I love my cat","You love my dog!","Do you think my dog is amazing?"]
@@ -53,7 +49,6 @@ class Request(Resource):
         return {"sentiment" : response}
 
 api.add_resource(Request,"/sentiment/<string:data>")
-=======
 def score(x):
     #1 bad, 2 neutral, 3 good
     token = tokenizer.encode(x,return_tensors ="pt")
@@ -71,7 +66,6 @@ class Request(Resource):
         return {"response":response}
 
 api.add_resource(Request,"/sentiment")
->>>>>>> cf5e1c883e2a08a149b2e3b2dcd36db8c7a7517f
 
 if __name__ == "__main__":
     app.run(debug=True,host="0.0.0.0", port=5000)
